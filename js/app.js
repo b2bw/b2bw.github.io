1cbaf96aec012b359e2bd136ce4a21009e03500d
phil <phil@branch14.org>
2018-04-09 18:30:04 +0200

    fix: indentation & newlines

--- js/app.js
+++ js/app.js
@@ -4,57 +4,52 @@ var raisedAmountWidth = 0,
     progressBar = document.getElementById("amountProgress");
 
 function scale(elem) {
-    // console.log("scaling the bar");
-    var s = currentAmount;
-    var punctuationless = s.replace(/[.,\/#!$%\^&\*;:{}='\-_`~()]/g,"");
-    var finalString = punctuationless.replace(/\s{2,}/g," ");
-    var raisedAmountWidth = ((finalString - 0) / (goalAmount - 0))*100;
-    var width = 0;
-    function frame() {
-	// console.log('width: ' + width + '%');
-	// console.log('raised: ' + raisedAmountWidth);
+  // console.log("scaling the bar");
+  var s = currentAmount;
+  var punctuationless = s.replace(/[.,\/#!$%\^&\*;:{}='\-_`~()]/g,"");
+  var finalString = punctuationless.replace(/\s{2,}/g," ");
+  var raisedAmountWidth = ((finalString - 0) / (goalAmount - 0))*100;
+  var width = 0;
+  function frame() {
+    // console.log('width: ' + width + '%');
+    // console.log('raised: ' + raisedAmountWidth);
 
-	if (width <= raisedAmountWidth && width < 100) {
-	    console.log(width)
-	    console.log('less than, shoudl proceed')
-	    width++; // update parameters
-	    elem.style.width = width + '%'; // show frame
+    if (width <= raisedAmountWidth && width < 100) {
+      console.log(width)
+      console.log('less than, shoudl proceed')
+      width++; // update parameters
+      elem.style.width = width + '%'; // show frame
     } else if (width >= raisedAmountWidth) {  // check finish condition
-	    elem.style.width =  '100%'; // show frame
-	    clearInterval(id);
-	    // console.log('done');
-	}
+      elem.style.width =  '100%'; // show frame
+      clearInterval(id);
+      // console.log('done');
     }
-    var id = setInterval(frame, 10) // draw every 10ms
+  }
+  var id = setInterval(frame, 10) // draw every 10ms
 }
 
-
-
-
 document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById("amountProgress").style.width = "0%";
-    scale(progressBar);
+  document.getElementById("amountProgress").style.width = "0%";
+  scale(progressBar);
 });
 
-
 var twitter = function(e){
-    e.preventDefault()
-    social_url = "https://twitter.com/intent/tweet?source=webclient&text=" + encodeURI(window.location + " Orang-Utans in die Freiheit! ") + "%23born2bewild";
-    window.open(social_url, "_blank").focus();
+  e.preventDefault()
+  social_url = "https://twitter.com/intent/tweet?source=webclient&text=" + encodeURI(window.location + " Orang-Utans in die Freiheit! ") + "%23born2bewild";
+  window.open(social_url, "_blank").focus();
 }
 
 var twitterVideo = function(e){
-    e.preventDefault();
-    var msg = String(document.getElementById("twitter-video-share-btn").getAttribute("data-share-msg"));
-    social_url = "https://twitter.com/intent/tweet?source=webclient&text=" + encodeURI(window.location + " " + msg + " ") + "%23born2bewild";
-    window.open(social_url, "_blank").focus();
+  e.preventDefault();
+  var msg = String(document.getElementById("twitter-video-share-btn").getAttribute("data-share-msg"));
+  social_url = "https://twitter.com/intent/tweet?source=webclient&text=" + encodeURI(window.location + " " + msg + " ") + "%23born2bewild";
+  window.open(social_url, "_blank").focus();
 }
 
-
 var facebook = function(e){
-    e.preventDefault()
-    social_url = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURI(window.location);
-    window.open(social_url, "_blank").focus();
+  e.preventDefault()
+  social_url = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURI(window.location);
+  window.open(social_url, "_blank").focus();
 }
 
 
@@ -68,17 +63,16 @@ var TLDiv = document.getElementsByClassName('top-left')[0]
 var TRDiv = document.getElementsByClassName('top-right')[0]
 
 function resizeTopRight(){
-    var TLHeight = TLDiv.offsetHeight
-    var TRHeight = TRDiv.offsetHeight
-    if(TRHeight <= TLHeight){
-	TRDiv.style.height= TLHeight+"px";
-    } else {
-	TRDiv.style.height = "auto";
-    }
+  var TLHeight = TLDiv.offsetHeight
+  var TRHeight = TRDiv.offsetHeight
+  if(TRHeight <= TLHeight){
+    TRDiv.style.height= TLHeight+"px";
+  } else {
+    TRDiv.style.height = "auto";
+  }
 }
 
 window.addEventListener('resize', resizeTopRight, false);
 resizeTopRight();
-
 
 rightHeight.init();
